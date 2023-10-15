a548000143192bf74dbe44167cc8cf450518e949
Tarannum khan <125120619+tarannum3824@users.noreply.github.com>
2023-10-15 15:31:59 +0530

    Add files via upload

---
+++ Day 7=Number mirror.java
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int N; // Declare an integer variable to store the input number.
+
+    // Read the input from the user
+    cin >> N;
+
+    // Print the input number as output
+    cout << N << endl;
+	return 0;
+}
